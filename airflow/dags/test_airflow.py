782373ae5c6b88ea430287cbaa8d3149c789b713
shawn.zx <zxforward@gmail.com>
2018-05-20 23:22:22 +0800

    test retry

--- airflow/dags/test_airflow.py
+++ airflow/dags/test_airflow.py
@@ -1,3 +1,4 @@
+import airflow
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
@@ -5,25 +6,26 @@ from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'opsbuild',
-    'depends_on_past': False,
-    'start_date': datetime(2015, 6, 1),
+    #'depends_on_past': False,
+    'start_date': airflow.utils.dates.days_ago(2),
     #'email': ['airflow@example.com'],
     #'email_on_failure': False,
     #'email_on_retry': False,
     #'retries': 1,
-    #'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(seconds=5),
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
 }
 
-dag = DAG('django-airflow', default_args=default_args)
+dag = DAG('django-airflow', default_args=default_args, schedule_interval=None)
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
 t1 = BashOperator(
-    task_id='print_date',
-    bash_command='date',
+    task_id='succ_task',
+    bash_command='echo "Trying task" && exit 0',
+    retries=2,
     dag=dag)
 
 t2 = BashOperator(
